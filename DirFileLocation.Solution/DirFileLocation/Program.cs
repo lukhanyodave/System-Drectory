b236c74508f16657a8e3a5215a736d8a509ab62e
lukhanyodave <lukhanyokumanda@gmail.com>
2023-06-07 20:47:58 +0000

    init

---
+++ DirFileLocation.Solution/DirFileLocation/Program.cs
@@ -0,0 +1,47 @@
+using DirFileLocation.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddTransient<IDirFileService, DirFileService>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//builder.Services.AddCors(options =>
+//{
+//    options.AddDefaultPolicy(builder =>
+//    {
+//        builder.AllowAnyOrigin()
+//               .AllowAnyHeader()
+//               .AllowAnyMethod();
+//    });
+//});
+
+//services cors
+builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+
+
+{
+    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("corsapp");
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
